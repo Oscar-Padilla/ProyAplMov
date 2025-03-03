d80820f03041fb1ede924b5f98fa5b0328bcdcf3
Oscar Padilla <72768285+Oscar-Padilla@users.noreply.github.com>
2025-03-03 10:29:41 -0600

    Implement IniciodeSesion_1.js

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import IniciodeSesion from './src/screens/IniciodeSesion';
+import IniciodeSesion_1 from './src/screens/IniciodeSesion_1';
+import { useState } from 'react';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View>
-      <IniciodeSesion />
+      <IniciodeSesion setModalVisible={setModalVisible}/>
+      <IniciodeSesion_1 modalVisible={modalVisible} setModalVisible={setModalVisible}/>
     </View>
   );
 }
