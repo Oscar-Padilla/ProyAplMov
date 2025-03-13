5eb50af0d84f25eec34aa075c8d162e171dbfba9
MichRQ <michellerq37@gmail.com>
2025-03-13 16:44:24 -0600

    Primer commit
    1-.Modificación de la navegación en App.js 
    2-. Modificación de botón siguiente en Registrate1.js
    3-. Creación de Registrate2 y Registrate 3

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import IniciodeSesion from './src/screens/IniciodeSesion';
 import Registrate from './src/screens/Registrate';
 import Registrate1 from './src/screens/Registrate1';
+import Registrate2 from './src/screens/Registrate2';
+import Registrate3 from './src/screens/Registrate3';
 
 const Stack = createStackNavigator();
 
@@ -13,6 +15,8 @@ export default function App() {
         <Stack.Screen name="IniciodeSesion" component={IniciodeSesion} options={{animation: 'slide_from_left'}}/>
         <Stack.Screen name="Registrate" component={Registrate} options={{animation: 'fade'}}/>
         <Stack.Screen name="Registrate1" component={Registrate1} options={{animation: 'fade', animationTypeForReplace:'pop'}}/>
+        <Stack.Screen name="Registrate2" component={Registrate2} options={{animation: 'fade', animationTypeForReplace: 'pop'}}/>
+        <Stack.Screen name="Registrate3" component={Registrate3} options={{animation: 'fade', animationTypeForReplace: 'pop'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
