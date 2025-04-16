12a490aa0d3669e140144424bf382560b98b54f8
Oscar Padilla <72768285+Oscar-Padilla@users.noreply.github.com>
2025-04-15 22:49:06 -0600

    start

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import Registrate from './src/screens/Registrate';
 import Registrate1 from './src/screens/Registrate1';
 import Registrate2 from './src/screens/Registrate2';
 import Registrate3 from './src/screens/Registrate3';
-import Home from './src/screens/Home';
+// import HomeMaestroStaff from './src/screens/HomeMaestroStaff';
+// import Home from './src/screens/Home';
 
 const Stack = createStackNavigator();
 
@@ -18,7 +19,7 @@ export default function App() {
         <Stack.Screen name="Registrate1" component={Registrate1} options={{animation: 'fade', animationTypeForReplace:'pop'}}/>
         <Stack.Screen name="Registrate2" component={Registrate2} options={{animation: 'fade', animationTypeForReplace: 'pop'}}/>
         <Stack.Screen name="Registrate3" component={Registrate3} options={{animation: 'fade', animationTypeForReplace: 'pop'}}/>
-        <Stack.Screen name="HomeMaestroStaff" component={HomeMaestroStaff} options={{animation: 'slide_from_left'}}/>
+        {/* <Stack.Screen name="HomeMaestroStaff" component={HomeMaestroStaff} options={{animation: 'slide_from_left'}}/> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
